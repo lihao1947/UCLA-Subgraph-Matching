a4053acf2efa50cc426262caffd7c3fa77431b67
hexie1995 <39319332+hexie1995@users.noreply.github.com>
2019-09-08 15:38:43 -0400

    Add files via upload

--- uclasm/noisy_counting/load_result.py
+++ uclasm/noisy_counting/load_result.py
@@ -8,19 +8,26 @@ from utils import data
 import uclasm
 import pickle
 import matplotlib.pyplot as plt
+from filters.attribute_filter_btn import attribute_filter
 from plotting.plot_noise import plot_lost_edge
 
 
 tmplts, world = data.pnnl_v6(0)
 tmplt = tmplts[0]
-
+tmplt.ch_to_adj['1'][2,9] = 4
 isomo = pickle.load(open("astar_btn.pl","rb"))
+
+
+
+signal = isomo[0].state
+miss, miss_dict, node_dict, correspond = uclasm.count_missing_edges(tmplt, world, signal)
+
 
 print("The index of the world candidates isomorphism you want is: ")
 print(world.nodes[isomo[0].state])
 print("This isomo has a cost of: ")
 print(isomo[0].loss)
 
-signal = isomo[0].state
-miss, miss_dict = uclasm.count_missing_edges(tmplt, world, signal)
-plot_lost_edge(tmplt, miss_dict, "test_lost_edge")
+# import IPython
+# IPython.embed()
+plot_lost_edge(tmplt, miss_dict, node_dict, "test_lost_edge")
